a97eb16b492b8c35f0df619e5837b4f32148ac79
Thushajoygit config --global user.name git config --global user.name Thushajoygit config --global user.name Thushajoy <armen@mkrtchian.fr>
2019-05-03 17:35:19 +0200

    d8.form.modify

---
+++ web/modules/custom/hello/src/Form/AdminForm.php
@@ -0,0 +1,50 @@
+<?php
+namespace Drupal\hello\Form;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+/**
+ *Implements a hello form
+ */
+class AdminForm extends ConfigFormBase{
+  /**
+   *{@inheritdoc}.
+   */
+  public function getFormID(){
+    return 'admin-form';
+  }
+  /**
+   *{@inheritdoc}.
+   */
+
+protected function getEditableConfigNames(){
+  return ['hello.settings'];
+}
+  /**
+   *{@inheritdoc}.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state){
+    $form['list'] = array(
+      '#type' => 'select',
+      '#title'=> 'List',
+      '#options'=>array(
+        '0' => '0 day',
+        '1'=> '1 day',
+        '2'=> '2 days',
+        '7'=> '7  days',
+        '14'=> '14 days',
+        '30'=> '30 days',
+      ),
+      '#default_value' => $this->config('hello.settings')->get('purge_days_number'),
+      '#attributes'=>['class'=>['list']]
+    );
+    return parent::buildForm($form, $form_state);
+  }
+  /**
+   *{@inheritdoc}.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state){
+    $purge_days_number = $form_state->getValue('list');
+    $this->config('hello.settings')->set('purge_days_number', $purge_days_number)->save();
+    parent::submitForm($form, $form_state);
+  }
+}
---
+++ web/modules/custom/hello/src/Form/HelloForm.php
@@ -0,0 +1,98 @@
+<?php
+namespace Drupal\hello\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+/**
+ *Implements a hello form
+ */
+class HelloForm extends FormBase{
+  /**
+   *{@inheritdoc}.
+   */
+  public function getFormID(){
+    return 'hello-form';
+  }
+  /**
+   *{@inheritdoc}.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state){
+    if(isset($form_state->getRebuildInfo()['result'])){
+      $form['form_state']=array(
+        '#type' => 'html_tag',
+        '#tag' =>'h2',
+        '#value' => 'Result '.$form_state->getRebuildInfo()['result']
+      );
+    }
+    $form['first_value'] = array(
+      '#type' => 'textfield',
+      '#title'=> 'FirstValue',
+    );
+    $form['operations'] = array(
+      '#type' => 'radios',
+      '#title'=> 'Operations',
+      '#options'=>array(
+        'Add' => 'Add',
+        'Subtraction'=> 'Subtraction',
+        'Multiplication'=>'Multiplication',
+        'Division'=>'Division',
+        ),
+      '#default_value'=>'10',
+      '#attributes'=>['class'=>['Operations']]
+    );
+    $form['second_value'] = array(
+      '#type' => 'textfield',
+      '#title'=> 'SecondValue',
+    );
+    $form['button'] = array(
+      '#type' => 'submit',
+      '#value'=> $this->t('Calculate'),
+    );
+    return $form;
+  }
+  
+  /**
+   *{@inheritdoc}.
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $First_Value = $form_state->getValue('first_value');
+    if (!is_numeric($First_Value)){
+      $form_state->setErrorByName('first_value', $this->t('First value Should Be Numeric!'));
+    }
+
+    $Second_Value = $form_state->getValue('second_value');
+    if (!is_numeric($Second_Value)) {
+      $form_state->setErrorByName('second_value', $this->t('Value 2 should Be Numeric!'));
+    }
+ 
+    $operation = $form_state->getValue('operations');
+    if ($Second_Value == '0' && $operation == 'Division') {
+      $form_state->setErrorByName('Second_Value', $this->t('Should Be not zero.'));
+    }
+
+    if (isset($form['result'])) {
+      unset($form['result']);
+    }
+  }
+
+  /**
+   *{@inheritdoc}.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state){
+    $First_Value=$form_state->getValue('first_value');
+    $Second_Value=$form_state->getValue('second_value');
+    $Operations=$form_state->getValue('operations');
+
+    if ($Operations == 'Add'){
+      $result = $First_Value + $Second_Value;
+    }elseif ($Operations == 'Subtraction'){
+      $result = $First_Value - $Second_Value;
+      }elseif ($Operations == 'Multiplication') {
+      $result = $First_Value * $Second_Value;
+    }else ($Operations == 'Division'){
+      $result = $First_Value / $Second_Value
+    };
+    $form_state->addRebuildInfo('result',$result);
+    $form_state->setRebuild();
+  }
+
+}
